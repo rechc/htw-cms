c279f004a1284729d5ee500071fe98b6f30ca7cc
Rech.Christian@gmail.com <Rech.Christian@gmail.com@b0215a42-db33-4ed6-ea50-885afab66677>
2011-12-27 14:13:31 +0000

    Projektordner

---
+++ trunk/bookAdvice/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='bookAdvice',
+      version=version,
+      description="Project for cms class. Contains content of book informations and recommendations",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bookAdvice'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ trunk/bookAdvice/src/bookAdvice/bookAdvice/README.txt
@@ -0,0 +1,14 @@
+bookAdvice Package Readme
+=========================
+
+Overview
+--------
+
+Project for cms class. Contains content of book informations and recommendations
+
+
+Your tests here
+---------------
+
+    >>> 1 + 1
+    3
